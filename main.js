0592eb741280a0114498ee158d9637d65c8463e5
mohamedabulfaiz <faizfahad1324@gmail.com>
2024-10-04 19:27:34 +0530

    finish

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var age = document.getElementById("age");
+var error = document.getElementById("error");
+var display= document.getElementById("display");
+
+function check(){
+    if (age.value === "" ){
+        error.innerHTML = "enter valid number";
+    } else{
+        error.innerHTML = "";
+        if (age.value >= 18){
+            display.innerHTML = "your allowed";
+        }else{
+            display.innerHTML=" your not allowed";
+        }
+    }
+    clear()
+    
+}
+
+function clear(){
+    age.value = ""
+}
